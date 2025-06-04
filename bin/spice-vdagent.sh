7531b53e9f55fa9a9a413c73cb168c394ed2dd7d
Piero Proietti <piero.proietti@gmail.com>
2025-06-04 22:06:41 +0200

    vdagent

--- bin/spice-vdagent.sh
+++ bin/spice-vdagent.sh
@@ -3,8 +3,8 @@ set -e
 
 # spice-vdagent per la condivisione degli appunti e il ridimensionamento della finestra
 apk add spice-vdagent
-rc-update add spice-vdagent default
-rc-service spice-vdagent start
+rc-update add spice-vdagentd default
+rc-service spice-vdagentd start
 # Attiva il servizio spice-vdagent
 if [ -f /etc/xdg/autostart/spice-vdagent.desktop ]; then
     sed -i 's/OnlyShowIn=GNOME;XFCE;/OnlyShowIn=GNOME;XFCE;X-Cinnamon;/' /etc/xdg/autostart/spice-vdagent.desktop
